ec633144c33b88a084b4e5a8fa434bfeef6bdae9
Potnuru-Jayavijaygopi <ponturu@paithacs.com>
2026-07-07 18:46:22 +0530

    up

---
+++ first-project/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+const Navbar = () => {
+  return (
+  <>
+  <nav class="navbar">
+    <div class="logo">MyLogo</div>
+
+<ul class="nav-links">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+  </>
+  )
+}
+
+export default Navbar
